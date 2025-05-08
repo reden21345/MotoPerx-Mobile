0d8abcc4e4babe923347d1b5a5d45aa7314f6421
reden21345 <mirandilla395@gmail.com>
2025-05-08 11:59:58 +0800

    Change URL

--- src/redux/actions/authAction.js
+++ src/redux/actions/authAction.js
@@ -2,7 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_BASE_URL = 'https://motoperx-backend.onrender.com/api/v1';
+const API_BASE_URL = 'http://192.168.100.100:5000/api/v1';
 
 // Async Thunks
 export const loginUser = createAsyncThunk('auth/loginUser', async ({ email, password }, thunkAPI) => {
