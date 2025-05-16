b225fa0b711d6822d253401b4e5cfb22b13b0c25
reden21345 <mirandilla395@gmail.com>
2025-05-16 13:54:00 +0800

    Change url to localhost

--- src/screens/user/sample.jsx
+++ src/screens/user/sample.jsx
@@ -21,7 +21,7 @@ const GpsLocation = () => {
   const fetchNearbyPartners = async (coords) => {
     try {
       const res = await axios.post(
-        "https://motoperx-backend.onrender.com/api/partners/nearby",
+        "http://192.168.100.100:5000/api/partners/nearby",
         {
           latitude: coords.latitude,
           longitude: coords.longitude,
