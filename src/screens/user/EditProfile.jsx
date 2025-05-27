065790ee857b76c3f669a0cbce866c4cec9fdbac
reden21345 <mirandilla395@gmail.com>
2025-05-27 14:33:50 +0800

    Update profile

--- src/screens/user/EditProfile.jsx
+++ src/screens/user/EditProfile.jsx
@@ -7,11 +7,22 @@ import {
   StyleSheet,
   Alert,
   Image,
+  Modal,
+  ActivityIndicator,
+  ScrollView,
 } from "react-native";
+import { GOOGLE_MAPS_API } from "@env";
 import * as ImagePicker from "expo-image-picker";
 import * as FileSystem from "expo-file-system";
+import * as Location from "expo-location";
+import MapView, { Marker } from "react-native-maps";
+import Geocoder from "react-native-geocoding";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import Ionicons from "react-native-vector-icons/Ionicons";
 import { useDispatch, useSelector } from "react-redux";
 import { editProfile } from "../../redux/actions/authAction";
+
+Geocoder.init(`${GOOGLE_MAPS_API}`);
 
 const EditProfile = ({ route, navigation }) => {
   const dispatch = useDispatch();
@@ -21,8 +32,91 @@ const EditProfile = ({ route, navigation }) => {
   const [name, setName] = useState(user?.name || "");
   const [email, setEmail] = useState(user?.email || "");
   const [phone, setPhone] = useState(user?.phone || "");
+  const [address, setAddress] = useState("No address...");
+  const [location, setLocation] = useState(
+    user?.location?.coordinates ? user.location.coordinates : null
+  );
   const [avatar, setAvatar] = useState(user?.avatar?.url || null);
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [region, setRegion] = useState(null);
+  const [editLoc, setEditLoc] = useState(false);
+
+  const [birthday, setBirthday] = useState(
+    user?.birthday ? new Date(user.birthday) : new Date()
+  );
+  const [showDatePicker, setShowDatePicker] = useState(false);
+
+  const [mapVisible, setMapVisible] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    const getAddress = async () => {
+      if (!location) return;
+
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.warn("Permission to access location was denied");
+        setAddress("Permission denied");
+        return;
+      }
+
+      const reverseRegion = {
+        latitude: location[1],
+        longitude: location[0],
+      };
+
+      try {
+        const response = await Location.reverseGeocodeAsync(reverseRegion);
+        if (response && response.length > 0) {
+          const { formattedAddress } = response[0];
+          setAddress(`${formattedAddress}`);
+        }
+      } catch (error) {
+        console.warn("Failed to reverse geocode:", error);
+        setAddress("Unknown");
+      }
+    };
+
+    getAddress();
+  }, [location]);
+
+  useEffect(() => {
+    const fetchCurrentLocation = async () => {
+      try {
+        const { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== "granted") {
+          Alert.alert("Permission Denied", "Location permission is required.");
+          return;
+        }
+
+        const { coords } = await Location.getCurrentPositionAsync({
+          accuracy: Location.Accuracy.High,
+        });
+
+        setRegion({
+          latitude: coords.latitude,
+          longitude: coords.longitude,
+          latitudeDelta: 0.001,
+          longitudeDelta: 0.001,
+        });
+      } catch (error) {
+        console.error("Error getting location:", error);
+      }
+    };
+
+    fetchCurrentLocation();
+  }, []);
+
+  const handleRegionChangeComplete = (newRegion) => {
+    setLocation([newRegion.longitude, newRegion.latitude]);
+    setRegion((prev) => ({
+      ...prev,
+      latitude: newRegion.latitude,
+      longitude: newRegion.longitude,
+      latitudeDelta: newRegion.latitudeDelta,
+      longitudeDelta: newRegion.longitudeDelta,
+    }));
+  };
 
   useEffect(() => {
     if (error) {
@@ -36,6 +130,8 @@ const EditProfile = ({ route, navigation }) => {
       email,
       phone,
       avatar,
+      birthday,
+      location,
     };
 
     if (!user.avatar?.url && !avatar) {
@@ -84,8 +180,35 @@ const EditProfile = ({ route, navigation }) => {
     }
   };
 
+  const handleSearch = async () => {
+    try {
+      const geoData = await Geocoder.from(searchQuery);
+      const { lat, lng } = geoData.results[0].geometry.location;
+      setLocation([lng, lat]);
+      setRegion({
+        latitude: lat,
+        longitude: lng,
+        latitudeDelta: 0.001,
+        longitudeDelta: 0.001,
+      });
+    } catch (error) {
+      console.error("Error during geocoding:", error);
+    }
+  };
+
+  const handleDateChange = (event, selectedDate) => {
+    setShowDatePicker(false);
+    if (selectedDate) {
+      setBirthday(selectedDate);
+    }
+  };
+
+  const formatDate = (date) => {
+    return date.toLocaleDateString();
+  };
+
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.title}>Edit Profile</Text>
 
       <TouchableOpacity onPress={pickAvatar} style={styles.avatarPicker}>
@@ -120,6 +243,74 @@ const EditProfile = ({ route, navigation }) => {
       />
 
       <TouchableOpacity
+        onPress={() => setShowDatePicker(true)}
+        style={styles.birthday}
+      >
+        <Text style={styles.datePickerText}>
+          Birthday: {formatDate(birthday)}
+        </Text>
+      </TouchableOpacity>
+
+      {showDatePicker && (
+        <DateTimePicker
+          value={birthday}
+          mode="date"
+          display="default"
+          onChange={handleDateChange}
+        />
+      )}
+
+      <View style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
+        <Text style={styles.subtext} numberOfLines={3} ellipsizeMode="tail">
+          {address}
+        </Text>
+        <TouchableOpacity onPress={() => setMapVisible(true)}>
+          <Ionicons name="map" size={20} color="#000" />
+        </TouchableOpacity>
+      </View>
+
+      <Modal visible={mapVisible} animationType="slide">
+        <View style={styles.searchContainer}>
+          <TextInput
+            style={styles.input2}
+            placeholder="Search Location"
+            value={searchQuery}
+            onChangeText={setSearchQuery}
+          />
+          <TouchableOpacity onPress={handleSearch} style={styles.searchButton}>
+            <Ionicons name="search" size={20} color="white" />
+          </TouchableOpacity>
+        </View>
+        {region ? (
+          <MapView
+            style={styles.map}
+            initialRegion={region}
+            onRegionChangeComplete={handleRegionChangeComplete}
+          >
+            <Marker coordinate={region} />
+          </MapView>
+        ) : (
+          <ActivityIndicator size="large" color="#424242" />
+        )}
+
+        <TouchableOpacity
+          onPress={() => {
+            setMapVisible(false);
+            setEditLoc(true);
+          }}
+          style={styles.saveButton}
+        >
+          <Text style={styles.saveButtonText}>Save</Text>
+        </TouchableOpacity>
+      </Modal>
+
+      {editLoc && (
+        <MapView style={styles.miniMap} region={region}>
+          <Marker coordinate={region} />
+        </MapView>
+      )}
+
+      <TouchableOpacity
         style={[styles.button, isSubmitting && { opacity: 0.6 }]}
         onPress={handleSubmit}
         disabled={isSubmitting}
@@ -130,16 +321,15 @@ const EditProfile = ({ route, navigation }) => {
           <Text style={styles.buttonText}>Save Profile</Text>
         )}
       </TouchableOpacity>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#fff",
     padding: 20,
-    justifyContent: "center",
+    paddingBottom: 30, // ensures room to scroll
   },
   title: {
     fontSize: 24,
@@ -184,6 +374,15 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
     fontSize: 16,
   },
+  birthday: {
+    height: 50,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 15,
+    paddingHorizontal: 15,
+    fontSize: 16,
+  },
   button: {
     backgroundColor: "#000",
     padding: 15,
@@ -192,8 +391,71 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     color: "#fff",
-    fontSize: 16,
+    fontSize: 17,
     fontWeight: "600",
+  },
+  input2: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: "#ddd",
+    padding: 8,
+    borderRadius: 5,
+  },
+  locBtn: {
+    backgroundColor: "#424242",
+    paddingVertical: 14,
+    paddingHorizontal: 30,
+    borderRadius: 10,
+    width: "50%",
+    alignItems: "center",
+    marginVertical: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  saveButton: {
+    position: "absolute", // Position it over the map
+    bottom: 30, // Distance from the bottom of the screen
+    alignSelf: "center", // Center horizontally
+    backgroundColor: "#424242",
+    paddingVertical: 14,
+    paddingHorizontal: 30,
+    borderRadius: 10,
+    width: "50%",
+    alignItems: "center",
+    zIndex: 10,
+  },
+  saveButtonText: {
+    color: "#fff",
+    fontSize: 17,
+    fontWeight: "600",
+  },
+  map: {
+    width: "100%",
+    height: "90%",
+  },
+  searchContainer: { flexDirection: "row", padding: 10 },
+  searchButton: {
+    backgroundColor: "#424242",
+    padding: 10,
+    marginLeft: 5,
+    borderRadius: 5,
+  },
+  miniMap: {
+    width: "100%",
+    height: 150,
+    marginVertical: 10,
+  },
+  datePickerText: {
+    color: "#000",
+    alignSelf: "center",
+  },
+  subtext: {
+    fontSize: 14,
+    color: "#666",
+    marginTop: 4,
+    flexWrap: "wrap",
+    maxWidth: 200,
   },
 });
 
