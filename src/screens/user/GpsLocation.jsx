808083e7df41c6feaaca9374ec6aed54ed8d27d3
reden21345 <mirandilla395@gmail.com>
2025-07-01 14:05:06 +0800

    flexGPS

--- src/screens/user/GpsLocation.jsx
+++ src/screens/user/GpsLocation.jsx
@@ -235,7 +235,7 @@ const { width, height } = Dimensions.get("window");
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    //backgroundColor: "#fff",
     alignItems: "center",
     paddingTop: 60,
   },
