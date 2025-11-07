55d1002504b52b408e3303cf07b18ad8afd80a30
reden21345 <mirandilla395@gmail.com>
2025-11-07 15:13:42 +0800

    auto loggedIn user

--- AppContent.js
+++ AppContent.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { enableScreens } from "react-native-screens";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useDispatch } from "react-redux";
+import { profile } from "./src/redux/actions/authAction";
+import { View, ActivityIndicator } from "react-native";
 import * as Linking from "expo-linking";
+import "react-native-gesture-handler";
+
 import Login from "./src/screens/Login";
 import Register from "./src/screens/Register";
 import Main from "./src/navigation/BottomTab";
@@ -15,7 +21,7 @@ import Notifications from "./src/screens/Notifications";
 import Settings from "./src/screens/Settings";
 import ActivateAccount from "./src/screens/user/ActivateAccount";
 
-// Users Import
+// Users
 import Apply from "./src/screens/user/ApplyPartnership";
 import EditProfile from "./src/screens/user/EditProfile";
 import EditPassword from "./src/screens/user/EditPassword";
@@ -28,15 +34,14 @@ import AllProducts from "./src/screens/Products";
 import AllStores from "./src/screens/Stores";
 import MyGears from "./src/screens/user/MyGears";
 import TrackHistory from "./src/screens/user/TrackHistory";
-
 import ProductDetails from "./src/components/ProductComponent";
 
-// Admin Import
+// Admin
 import EditUser from "./src/screens/admin/EditUser";
 import PartnerDetails from "./src/screens/admin/PartnerDetails";
 import NotifyUsers from "./src/screens/admin/NotifyUsers";
 
-// Partner Import
+// Partner
 import CreateDeal from "./src/screens/partner/CreateDeal";
 import EditDeal from "./src/screens/partner/EditDeal";
 import Employees from "./src/screens/partner/Employees";
@@ -46,17 +51,17 @@ import AddProduct from "./src/screens/partner/AddProduct";
 import EditProduct from "./src/screens/partner/EditProduct";
 import EditPartner from "./src/screens/partner/EditPartner";
 
-// Post Imports
+// Posts
 import HomePosts from "./src/screens/post/HomePost";
 import PostDetails from "./src/screens/post/PostDetails";
 
-// Community Imports
+// Communities
 import UsersCommunities from "./src/screens/community/Communities";
 import CommunityDetails from "./src/screens/community/CommuniyDetails";
 import CreateCommunity from "./src/screens/community/CreateCommunity";
 import CommunityReports from "./src/screens/community/CommunityReports";
 
-// Game Imports
+// Games
 import GameMenu from "./src/screens/games/GameMenu";
 import ReactionGame from "./src/screens/games/ReactionGame";
 import QuizGame from "./src/screens/games/QuizGame";
@@ -64,8 +69,6 @@ import FuelGame from "./src/screens/games/FuelGame";
 
 // Dashboard
 import Shop from "./src/screens/dashboard/PartnerDashboard";
-
-import "react-native-gesture-handler";
 
 enableScreens();
 
@@ -82,14 +85,61 @@ const linking = {
 };
 
 const AppContent = () => {
+  const [initialRoute, setInitialRoute] = useState(null);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        const token = await AsyncStorage.getItem("token");
+
+        if (!token) {
+          setInitialRoute("Landing");
+          return;
+        }
+
+        const result = await dispatch(profile()).unwrap();
+
+        if (result?.success) {
+          setInitialRoute("Main");
+        } else {
+          await AsyncStorage.removeItem("token");
+          setInitialRoute("Landing");
+        }
+      } catch (error) {
+        console.log("Error verifying token:", error);
+        await AsyncStorage.removeItem("token");
+        setInitialRoute("Landing");
+      }
+    };
+
+    checkLoginStatus();
+  }, [dispatch]);
+
+  if (initialRoute === null) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#fff",
+        }}
+      >
+        <ActivityIndicator size="large" color="#000" />
+      </View>
+    );
+  }
+
   return (
     <NavigationContainer linking={linking}>
       <Stack.Navigator
-        initialRouteName="Landing"
+        initialRouteName={initialRoute}
         screenOptions={{
           header: (props) => <Header {...props} />,
         }}
       >
+        {/* Auth & Landing */}
         <Stack.Screen
           name="Landing"
           component={Landing}
@@ -120,6 +170,8 @@ const AppContent = () => {
           component={ActivateAccount}
           options={{ headerShown: false }}
         />
+
+        {/* Main App */}
         <Stack.Screen name="Main" component={Main} />
         <Stack.Screen name="Apply" component={Apply} />
         <Stack.Screen name="Deals" component={Deals} />
@@ -129,7 +181,7 @@ const AppContent = () => {
         <Stack.Screen name="Notifications" component={Notifications} />
         <Stack.Screen name="Settings" component={Settings} />
 
-        {/* Profile Screens */}
+        {/* Profile */}
         <Stack.Screen name="EditProfile" component={EditProfile} />
         <Stack.Screen name="EditPassword" component={EditPassword} />
         <Stack.Screen name="GpsLocation" component={GpsLocation} />
@@ -138,15 +190,14 @@ const AppContent = () => {
         <Stack.Screen name="MyDeals" component={MyDeals} />
         <Stack.Screen name="MyGears" component={MyGears} />
         <Stack.Screen name="TrackHistory" component={TrackHistory} />
-
         <Stack.Screen name="ProductDetails" component={ProductDetails} />
 
-        {/* Admin Screens */}
+        {/* Admin */}
         <Stack.Screen name="EditUser" component={EditUser} />
         <Stack.Screen name="PartnerDetails" component={PartnerDetails} />
         <Stack.Screen name="Broadcast" component={NotifyUsers} />
 
-        {/* Partner Screens */}
+        {/* Partner */}
         <Stack.Screen name="CreateDeal" component={CreateDeal} />
         <Stack.Screen name="EditDeal" component={EditDeal} />
         <Stack.Screen name="Employees" component={Employees} />
@@ -157,7 +208,7 @@ const AppContent = () => {
         <Stack.Screen name="EditPartner" component={EditPartner} />
         <Stack.Screen name="Shop" component={Shop} />
 
-        {/* Partner Screens */}
+        {/* Posts */}
         <Stack.Screen name="HomePosts" component={HomePosts} />
         <Stack.Screen
           name="PostDetails"
@@ -165,7 +216,7 @@ const AppContent = () => {
           options={{ headerShown: false }}
         />
 
-        {/* Community Screens */}
+        {/* Communities */}
         <Stack.Screen name="UsersCommunities" component={UsersCommunities} />
         <Stack.Screen
           name="CommunityDetails"
@@ -179,7 +230,7 @@ const AppContent = () => {
           options={{ headerShown: false }}
         />
 
-        {/* Game Screens */}
+        {/* Games */}
         <Stack.Screen
           name="GameMenu"
           component={GameMenu}
